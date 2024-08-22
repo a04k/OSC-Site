c06f98a1de18cfc0bb994b45876d7b9ef895d573
Ahmed Khaled <63ahmedkhaled@gmail.com>
2024-08-23 00:00:28 +0300

    Fixing Prod bugs, and really terrible css

--- src/Components/postCard.jsx
+++ src/Components/postCard.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/stylesheets/postCard.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/stylesheets/postCard.css";
 
-export default function PostCard({ title, author, description, postDate, link }) {
+export default function PostCard({
+  title,
+  author,
+  description,
+  postDate,
+  link,
+}) {
   return (
-    <Link to={link} className='postCard'>
+    <Link to={link} className="postCard">
       <div className="image">
         <img src="src/assets/images/chevrolet_corvette.jpg" alt="" />
       </div>
       <div className="body">
-        <h2 className='ttl'>
-          {title}
-        </h2>
-        <p className="info">{author}, {postDate}</p>
-        <p className="description">
-          {description}
+        <h2 className="ttl">{title}</h2>
+        <p className="author-date-info">
+          {author}, {postDate}
         </p>
+        <p className="description">{description}</p>
       </div>
       <div className="arr"></div>
     </Link>
