1d12f3f7c388bddbb4e0254a7f01a145a3a73838
Ahmed Khaled <63ahmedkhaled@gmail.com>
2024-08-27 00:28:27 +0300

    Added Sticky Nav ~ Closes #19

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import '../assets/stylesheets/nav.scss';
 import logo from '../assets/images/logo.svg';
 import fb from '../assets/images/fb.svg';
@@ -8,8 +9,8 @@ import li from '../assets/images/linkedin.svg';
 export default function Navbar() {
   const location = useLocation();
   const currentLocation = location.pathname.toLowerCase();
+  const [scrolled, setScrolled] = useState(false);
 
-  // Function to check if the current location is the home page
   const isHome = () => currentLocation === '/';
   
   const handleScroll = () => {
@@ -17,24 +18,72 @@ export default function Navbar() {
     secTwo.scrollIntoView({ behavior: 'smooth' }); 
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = window.innerHeight * 0.02;
+      const isScrolled = window.scrollY > scrollThreshold;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
   return (
-    <nav>
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="OSC Logo" /> 
-        </Link>
-      </div>
-      <ul>
-        {/* Conditionally change the "About" link function based on whether we're on the home page */}
-        {isHome() ? <li onClick={handleScroll}><a>About</a></li > : <li><Link to="/about">About</Link></li>}
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/committees">Committees</Link></li>
-      </ul>
-      <div className='socials'>
-        <a href="https://github.com/Open-Source-Community" target= "_blank"><img src={gh} alt="GitHub" /></a>
-        <a href="https://www.linkedin.com/company/osc---open-source-community/" target= "_blank"><img src={li} alt="LinkedIn" /></a>
-        <a href="https://www.facebook.com/oscgeeks" target= "_blank"><img src={fb} alt="Facebook" /></a>
+    <nav className={scrolled ? 'scrolled' : ''}>
+      <div className="nav-content">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="OSC Logo" />
+          </Link>
+        </div>
+        <ul>
+          {isHome() ? (
+            <li onClick={handleScroll}>
+              <a>About</a>
+            </li>
+          ) : (
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          )}
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/committees">Committees</Link>
+          </li>
+        </ul>
+        <div className="socials">
+          <a
+            href="https://github.com/Open-Source-Community"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={gh} alt="GitHub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/osc---open-source-community/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={li} alt="LinkedIn" />
+          </a>
+          <a
+            href="https://www.facebook.com/oscgeeks"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={fb} alt="Facebook" />
+          </a>
+        </div>
       </div>
     </nav>
   );
+  
 }
